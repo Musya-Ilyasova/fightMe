083a6afb23b8081342d8b937b2fdb0c2393f386d
Musya <musya.ilyasova@getorca.app>
2023-08-11 16:40:41 +0300

    add

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 import modal from "./modules/modal"
-import addSlider from "./modules/slider";
+import { addSlider } from "./modules/slider";
 import copyClipboard from "./modules/copyClipboard";
 import inputAutoHeight from "./modules/inputAutoHeight";
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  // modal();
-  // addSlider();
-  // copyClipboard();
-  // inputAutoHeight();
+  modal();
+  addSlider();
+  copyClipboard();
+  inputAutoHeight();
 })
--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,9 +1,8 @@
 import KeenSlider from 'keen-slider';
 
-const addSlider = () => {
+export const addSlider = () => {
   const arrowLeft = document.querySelector(".slider__left");
   const arrowRight = document.querySelector(".slider__right");
-  const sliderWrapper = document.querySelector(".slider__wrapper")
   var slider = new KeenSlider (
     '#my-slider',
     {
@@ -40,6 +39,45 @@ const addSlider = () => {
   arrowRight.addEventListener("click", () => slider.next());
 }
 
-export default addSlider;
+
+export const addHalfSlider = () => {
+  const arrowLeft = document.querySelector(".slider__left");
+  const arrowRight = document.querySelector(".slider__right");
+  var slider = new KeenSlider (
+    '#personalprizes-slider',
+    {
+      loop: false,
+      mode: "free-snap",
+      slides: {
+        perView: "auto",
+        spacing: 12
+      },
+      range: {
+        max: 6,
+        align: false
+      }
+    }
+  )
+  slider.on("optionsChanged", () => {
+    updateClasses()
+  })
+  slider.on("slideChanged", () => {
+    updateClasses()
+  })
+
+  function updateClasses() {
+    var slide = slider.track.details.rel;
+    console.log(slider.track.details)
+    slide === 0
+      ? arrowLeft.classList.add("slider__arrow_disabled")
+      : arrowLeft.classList.remove("slider__arrow_disabled")
+    slide === 4
+      ? arrowRight.classList.add("slider__arrow_disabled")
+      : arrowRight.classList.remove("slider__arrow_disabled")
+  }
+  arrowLeft.addEventListener("click", () => slider.prev());
+  arrowRight.addEventListener("click", () => slider.next());
+}
+
 
 
